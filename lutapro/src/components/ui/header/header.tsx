5cfa92c1a601ddf1cb834fb7c9be97932802fb12
Fredy228 <smagrovich58@gmail.com>
2024-05-12 20:01:55 +0300

    startup 1.0

---
+++ lutapro/src/components/ui/header/header.tsx
@@ -0,0 +1,9 @@
+import { type FC } from "react";
+
+import styles from "./header.module.scss";
+
+const Header: FC = () => {
+  return <header className={styles.header}></header>;
+};
+
+export default Header;
